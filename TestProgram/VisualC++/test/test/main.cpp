c14057122678d84016c8a6dc92635e24385d212f
Sirawit <popeye-zz@hotmail.com>
2022-01-09 23:32:21 +0700

    save all files

--- TestProgram/VisualC++/test/test/main.cpp
+++ TestProgram/VisualC++/test/test/main.cpp
@@ -2,7 +2,10 @@
 
 int main(){
 
-	printf("test\n");
+	float x;
+	scanf("%d", &x);
+	printf("x = %f\n\n", x);
+	printf("%f",7/6+2-3*9*5*16);
 	return 0;
 
 }
---
+++ trashValue.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	
+	float w = 7/6+2-3*9*5*16;
+	printf("%f\n", w);
+
+	int i=0;
+	i+6;
+	printf("%d", i);
+	return 0;
+  
+  
+
+
+	
+}
